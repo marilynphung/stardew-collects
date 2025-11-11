5b3d6c904af5480eae7dbd517373b4f01814e81b
marilynphung <marilynphung@hotmail.com>
2025-11-10 23:49:11 -0500

    Completion of Stardew Collects 1.0

---
+++ src/index.js
@@ -0,0 +1,16 @@
+document.getElementById("wiki").addEventListener("click", (e) => {
+  window.location.href = "https://stardewvalleywiki.com/Stardew_Valley_Wiki";
+});
+
+document.getElementById("profit").addEventListener("click", (e) => {
+  window.location.href =
+    "https://thorinair.github.io/Stardew-Profits/#produce_0-equipment_0-sellRaw_false-sellExcess_false-aging_0-planted_1-maxSeedMoney_0-days_28-fertilizer_0-level_0-season_0-buySeed_false-replant_false-nextyear_false-buyFert_false-average_0-fertilizerSource_0-seeds_(pierre_true-joja_true-special_true)-skills_(till_false-agri_false-arti_false-gatherer_false-botanist_false)-foodIndex_0-foodLevel_0-extra_true-disableLinks_false-byHarvest_false-crossSeason_false-foragingLevel_0";
+});
+
+document.getElementById("farm_planner").addEventListener("click", (e) => {
+  window.location.href = "https://stardew.info/";
+});
+
+document.getElementById("dress_up").addEventListener("click", (e) => {
+  window.location.href = "https://lybell-art.github.io/stardew-dressup/";
+});
